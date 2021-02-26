c0c2346a0afff7f7fc14bda7311759d09b07375a
0xd34d10cc <0xd34d10cc@gmail.com>
2021-02-26 02:31:25 +0000

    Compile asm files to executables (win64 only)

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import sys
+import os
+import subprocess
+import shutil
+
 import bc
 import syntax
 
 
+def find_winsdk():
+    base = r'C:\Program Files (x86)\Windows Kits\10\Lib'
+
+    def version(path):
+        return [int(part) for part in os.path.basename(path).split('.')]
+
+    latest_sdk = max(os.listdir(base), key=version)
+    return os.path.join(base, latest_sdk)
+
+def assemble(program):
+    assert os.name == 'nt', 'Assembly compilation for {os.name} is not implemented'
+    assembler = shutil.which('nasm')
+    linker = shutil.which('lld-link') or shutil.which('link')
+    assert assembler, 'nasm is not in PATH'
+    assert linker, 'linker is not in PATH'
+
+    subprocess.run([assembler, '-f', 'win64', program], check=True)
+    obj = os.path.splitext(program)[0] + '.obj'
+    kernel32 = os.path.join(find_winsdk(), 'um', 'x64', 'kernel32.lib')
+    subprocess.run([linker, obj, '/subsystem:console', '/entry:_main', kernel32], check=True)
+    os.remove(obj)
+
 def main(file):
+    if file.endswith('.s') or file.endswith('.asm'):
+        assemble(file)
+        return
+
     with open(file, 'rt', encoding='utf-8') as f:
         program = f.read()
 
