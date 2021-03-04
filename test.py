d5deba1d58dedc1c4963ef3447d485ab1db2268b
0xd34d10cc <0xd34d10cc@gmail.com>
2021-03-04 23:33:27 +0000

    Use pytest.parametrize instead of hack with globals() to generate multiple tests

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import io
 import contextlib
 import subprocess
 
+import pytest
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(current_dir, '..'))
 
@@ -27,13 +29,14 @@ def read_files(base):
             data += (f.read(),)
     return data
 
-def run_case(test_case):
-    program, inp, expected_output = read_files(test_case)
+@pytest.mark.parametrize('file', glob.glob(os.path.join(current_dir, 'tests', '*.nox')))
+def test_program(file):
+    program, inp, expected_output = read_files(file)
     program = syntax.parse(program)
     program = bc.compile(program)
     assert program == bc.parse(str(program))
 
-    bytecode_file = test_case.replace('.nox', '.noxbc')
+    bytecode_file = file.replace('.nox', '.noxbc')
     with open(bytecode_file, 'wt') as f:
         f.write(str(program))
 
@@ -46,16 +49,11 @@ def run_case(test_case):
     assert expected_output == out.getvalue()
 
     asm = x64.compile(program)
-    asm_file = test_case.replace('.nox', '.s')
+    asm_file = file.replace('.nox', '.s')
     with open(asm_file, 'wt') as f:
         f.write(asm)
 
     driver.build(asm_file)
-    binary = test_case.replace('.nox', '.exe')
+    binary = file.replace('.nox', '.exe')
     status = subprocess.run([binary], input=inp.encode(), capture_output=True, timeout=0.5, check=True)
     assert expected_output == status.stdout.decode()
-
-cases = glob.glob(os.path.join(current_dir, 'tests', '*.nox'))
-for test_case in cases:
-    name, ext = os.path.basename(test_case).split('.')
-    globals()[f'test_{name}'] = lambda case=test_case: run_case(case)
